0b25f6fddab0bba4a2f7c4789170f3f0fe0ccc4e
TorresBones <jonathantorres521@gmail.com>
2023-10-17 18:32:35 -0400

    Fixing CSS

--- assets/script.js
+++ assets/script.js
@@ -1,110 +1,117 @@
 var questions = [
     {
         question: "Commongly used data types do not include:",
-        options: ["strings", "booleans", "alerts", "numbers"],
-        answer: 2,
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: 2,
     },
     {
         question: "The condition in an if/else statement is enclosed with ______.",
-        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-        answer: 2,
+        answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: 2,
     },
     {
         question: "Arrays in Javascript can be used to store ______.",
-        options: ["numbers and strings", "booleans", "other arrays", "all of tha above"],
-        answer: 3,
+        answers: ["numbers and strings", "booleans", "other arrays", "all of tha above"],
+        correct: 3,
     },
     {
         question: "Strings values must be enclosed within _____, when being assigned to variables.",
-        options: ["quotes", "curly brackets", "commas", "parenthesis"],
-        answer: 0,
+        answers: ["quotes", "curly brackets", "commas", "parenthesis"],
+        correct: 0,
     },
     {
-        question: "A very useful toll used during development and debugging for printing content to the debugger is:",
-        options: ["Javascript", "Terminal/Bash", "for loops", "console.log"],
-        answer: 3,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["Javascript", "Terminal/Bash", "for loops", "console.log"],
+        correct: 3,
     },
 ]
 
-var quizContainer = document.getElementById("quiz");
-var startButton = document.getElementById("Start-button");
-var timerElement = document.getElementById("timer");
-var submitButton = document.getElementById("submit");
-var questionFeedback = document.getElementById("question")
-var resultElement = document.getElementById("result");
-var scoreElement = document.getElementById("score");
-var initialsInput = document.getElementById("initials");
 
 var currentQuestion = 0;
 var score = 0;
 var timeLeft = 60;
 var timerInterval;
-var submitted = false;
+var initialsInput = document.getElementById("initials");
+var finalScore = document.getElementById("final-score");
+
+var startButton = document.getElementById("start-quiz");
+startButton.addEventListener("click", function() {
+    startQuiz();
+});
+
+var highscoresButton = document.getElementById("view-highscores");
+highscoresButton.addEventListener("click", function() {
+});
+
+var submitButton = document.getElementById("submit-score");
+submitButton.addEventListener("click", function() {
+
+    var initials = initialsInput.value;
+   
+});
 
 function startQuiz() {
-    startButton.style.display = "none";
-    quizContainer.style.display = "block";
-    showQuestion();
+    var startPage = document.getElementById("start-page");
+    var quizPage = document.getElementById("quiz");
+
+    startPage.style.display = "none";
+    quizPage.style.display = "block";
+
+    displayQuestion();
     startTimer();
 }
-function showQuestion() {
+
+function displayQuestion() {
+    var question = questions[currentQuestion];
+    var questionElement = document.getElementById("question");
+    questionElement.textContent = question.question;
+
+    var answerButtons = document.querySelectorAll(".answer");
+    for (var i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].textContent = question.answers[i];
+        answerButtons[i].addEventListener("click", function() {
+            checkAnswer(this.textContent);
+        });
+    }
+}
+
+function checkAnswer(selectedAnswer) {
+    var question = questions[currentQuestion];
+    if (selectedAnswer === question.answers[question.correct]) {
+        score++;
+    } else {
+        timeLeft -= 5; 
+    }
+    currentQuestion++;
+
     if (currentQuestion < questions.length) {
-        var questionData = questions[currentQuestion];
-        var options = questionData.options
-
-        for(var i = 0; i < options.length; i++){
-            console.log(options[i])
-        document.createElement()
-        }
-
+        displayQuestion();
     } else {
         finishQuiz();
     }
 }
 
-startButton.addEventListener("click", startQuiz)
-
 function startTimer() {
+    var timeLeftElement = document.getElementById("time-left");
     timerInterval = setInterval(function() {
         timeLeft--;
-        timerElement.textContent = timeLeft;
+        timeLeftElement.textContent = timeLeft;
         if (timeLeft <= 0) {
-            finishQuiz ();
+            finishQuiz();
         }
     }, 1000);
+    
+    quizPage.style.display = "block";
+    timeLeftElement.style.display = "block";
 }
+
 
 function finishQuiz() {
     clearInterval(timerInterval);
-    quizContainer.style.display = "none";
-    if (!submitted) {
-        submitButton.style.display = "block";
-        initialsInput.style.display = "block";
-    }
-}
+    var quizPage = document.getElementById("quiz");
+    var scoreInputPage = document.getElementById("score-input");
 
-function evaluateAnswer() {
-    var selectedOption = document.querySelector("input[name='option']:checked");
-    if (selectedOption) {
-        var selectedAnswer = parseInt(selectedOption.value);
-        if (selectedAnswer === questions[currentQuestion].answer) {
-            score ++;
-            questionFeedback.textContent = "Correct!";
-        } else {
-            questionFeedback.textContent = "Incorrect!";
-            timeLeft -= 10;
-        }
-        currentQuestion++;
-        showQuestion();
-        selectedOption.checked = false;
-    }
+    quizPage.style.display = "none";
+    scoreInputPage.style.display = "block";
+    finalScore.textContent = score;
 }
-
-submitButton.addEventListener("click", function () {
-    if (!submitted) {
-        submitted = true;
-        initialsInput.disabled = true;
-        submitButton.style.display = "none";
-        scoreElement.textContent = score;
-    }
-});
