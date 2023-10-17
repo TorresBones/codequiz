506222583949cfae8f0207afef03c631dcdfae35
TorresBones <jonathantorres521@gmail.com>
2023-10-16 20:16:45 -0400

    Adiing Array of question to JS

--- assets/script.js
+++ assets/script.js
@@ -1,87 +1,29 @@
-console.log ("connected");
-
-const question = [
+var questions = [
     {
         question: "Commongly used data types do not include:",
-        answers: [
-            {Text: "strings", correct: false},
-            {Text: "booleans", correct: false},
-            {Text: "alerts", correct: true},
-            {Text: "numbers", correct: false},
-        ]
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: 2,
     },
     {
         question: "The condition in an if/else statement is enclosed with ______.",
-        answers: [
-            {Text: "quotes", correct: false},
-            {Text: "curly brackets", correct: false},
-            {Text: "parenthesis", correct: true},
-            {Text: "square brackets", correct: false},
-        ]
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: 2,
     },
     {
         question: "Arrays in Javascript can be used to store ______.",
-        answers: [
-            {Text: "number and strings", correct: false},
-            {Text: "booleans", correct: false},
-            {Text: "other arrays", correct: false},
-            {Text: "all of the above", correct: true},
-        ]
+        options: ["numbers and strings", "booleans", "other arrays", "all of tha above"],
+        answer: 3,
     },
     {
         question: "Strings values must be enclosed within _____, when being assigned to variables.",
-        answers: [
-            {Text: "commas", correct: false},
-            {Text: "curly brackets", correct: false},
-            {Text: "quotes", correct: true},
-            {Text: "parenthesis", correct: false},
-        ]
+        options: ["quotes", "curly brackets", "commas", "parenthesis"],
+        answer: 0,
     },
     {
         question: "A very useful toll used during development and debugging for printing content to the debugger is:",
-        answers: [
-            {Text: "Javascript", correct: false},
-            {Text: "terminal/bash", correct: false},
-            {Text: "for loops", correct: false},
-            {Text: "console.log", correct: true},
-        ]
-    }
+        options: ["Javascript", "Terminal/Bash", "for loops", "console.log"],
+        answer: 3,
+    },
 ]
 
-const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-button");
-
-let currrentQuestionIndex = 0;
-let score = 0;
-
-function startQuiz(){
-    currrentQuestionIndex = 0;
-    score = 0;
-    showQuestion();
-}
-
-function showQuestion() {
-    resetState();
-    let currentQuestion = questions[currrentQuestionIndex];
-    let questionNbr = currrentQuestionIndex + 1;
-    questionElement.innerHTML = questionNbr + "." + currentQuestion.question;
-
-    currentQuestion.Question.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButton.appendChild(button);
-        if(answer.correct){
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    });
-}
-function resetState(){
-    nextButton.style.display = "none";
-    while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
-}
-
-
+var quizContainer = document.getElementById("quiz")
